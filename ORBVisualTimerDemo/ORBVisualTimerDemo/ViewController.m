9b581ed9f1dc7cd6d43044b21645b3512b4ac48e
Vlad Averin <hello@vladaverin.me>
2017-03-08 01:38:15 +0300

    Update Demo project + replace screenshots.

--- ORBVisualTimerDemo/ORBVisualTimerDemo/ViewController.m
+++ ORBVisualTimerDemo/ORBVisualTimerDemo/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController () <ORBVisualTimerDelegate>
 
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *sampleViews;
+
 @end
 
 @implementation ViewController
@@ -24,8 +26,26 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+    [self prepareUI];
+    
     //[self minimalDemo];
     [self comprehensiveDemo];
+}
+
+#pragma mark - UI
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+- (void)prepareUI {
+    for (UIView *container in self.sampleViews) {
+        if (container == self.sampleViews.lastObject) { return; }
+        
+        UIView *sep = [[UIView alloc] initWithFrame:CGRectMake(0, container.bounds.size.height - 1, container.bounds.size.width, 1)];
+        sep.backgroundColor = [UIColor whiteColor];
+        [container addSubview:sep];
+    }
 }
 
 #pragma mark - Demo cases
@@ -76,62 +96,75 @@
 
 - (void)comprehensiveDemo {
     NSUInteger numOfBars = 3;
-    CGFloat availableHeight = self.view.bounds.size.height / 3 + 20 * (numOfBars - 1);
-    CGFloat maxBarHeight = availableHeight / numOfBars;
-    
-    UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0, 0,
-                                                                 self.view.bounds.size.width, availableHeight)];
-    container.center = self.view.center;
-    [self.view addSubview:container];
     
     for (NSUInteger i = 0; i < numOfBars; i++) {
-        CGRect barFrame = CGRectMake(10,
-                                     0 + (maxBarHeight + 20) * i,
-                                     container.bounds.size.width - 20,
-                                     maxBarHeight);
+        CGRect containerBounds = [self.sampleViews[i] bounds];
         
-        ORBVisualTimerBar *barTimer = (ORBVisualTimerBar *)[ORBVisualTimer timerWithStyle:ORBVisualTimerStyleBar frame:barFrame timeRemaining:(5.0f * (i+1))];
-        
-        barTimer.delegate = self;
-        
-        barTimer.tag = i;
+        ORBVisualTimerBar *barTimer;
         
         switch (i) {
             case 0: {
+                CGRect barFrame = CGRectMake(0, 0,
+                                             containerBounds.size.width,
+                                             containerBounds.size.height * 0.24);
+                
+                barTimer = (ORBVisualTimerBar *)[ORBVisualTimer timerWithStyle:ORBVisualTimerStyleBar frame:barFrame timeRemaining:(5)];
+                
+                barTimer.center = CGPointMake(containerBounds.size.width / 2,
+                                              containerBounds.size.height * 0.88);
+                
                 barTimer.barAnimationStyle = ORBVisualTimerBarAnimationStyleStraight;
                 
-                barTimer.backgroundViewColor = [UIColor darkGrayColor];
-                barTimer.backgroundViewCornerRadius = 20.0f;
-                barTimer.timerShapeInactiveColor = [UIColor lightGrayColor];
-                barTimer.timerShapeActiveColor = [UIColor colorWithRed:0.0f green:1.0f blue:0.3f alpha:0.8f];
+                barTimer.backgroundViewColor = [UIColor colorWithWhite:0.0f alpha:0.40f];
+                barTimer.backgroundViewCornerRadius = 0.0f;
+                barTimer.timerShapeInactiveColor = [UIColor darkGrayColor];
+                barTimer.timerShapeActiveColor = [UIColor orangeColor];
                 barTimer.timerLabelColor = [UIColor whiteColor];
                 //barTimer.showTimerLabel = NO;
                 //barTimer.autohideWhenFired = YES;
-                //barTimer.barCapStyle = kCALineCapSquare;
-                barTimer.barThickness = 20.0f;
+                barTimer.barCapStyle = kCALineCapSquare;
+                barTimer.barThickness = 10.0f;
                 barTimer.barPadding = 20.0f;
                 
                 break;
             }
                 
             case 1: {
+                CGRect barFrame = CGRectMake(0, 0,
+                                             containerBounds.size.width * 0.80,
+                                             containerBounds.size.height * 0.4);
+                
+                barTimer = (ORBVisualTimerBar *)[ORBVisualTimer timerWithStyle:ORBVisualTimerStyleBar frame:barFrame timeRemaining:(7)];
+                
+                barTimer.center = CGPointMake(containerBounds.size.width / 2,
+                                              containerBounds.size.height * 0.5);
+                
                 barTimer.barAnimationStyle = ORBVisualTimerBarAnimationStyleReflection;
                 
-                barTimer.backgroundViewColor = [UIColor colorWithWhite:0.9 alpha:0.9f];
+                barTimer.backgroundViewColor = [UIColor clearColor];
                 //barTimer.backgroundViewCornerRadius = 20.0f;
-                barTimer.timerShapeInactiveColor = [UIColor whiteColor];
+                barTimer.timerShapeInactiveColor = [UIColor colorWithWhite:1.0f alpha:0.4f];
                 barTimer.timerShapeActiveColor = [UIColor colorWithRed:0.0f green:0.0f blue:0.9f alpha:0.2f];
                 //barTimer.timerLabelColor = [UIColor whiteColor];
                 barTimer.showTimerLabel = NO;
                 //barTimer.autohideWhenFired = YES;
-                barTimer.barCapStyle = kCALineCapSquare;
-                barTimer.barThickness = 60.0f;
-                barTimer.barPadding = 50.0f;
+                barTimer.barCapStyle = kCALineCapRound;
+                barTimer.barThickness = 30.0f;
+                barTimer.barPadding = 10.0f;
                 
                 break;
             }
                 
             case 2: {
+                CGRect barFrame = CGRectMake(0, 0,
+                                             containerBounds.size.width * 0.50,
+                                             containerBounds.size.height * 0.3);
+                
+                barTimer = (ORBVisualTimerBar *)[ORBVisualTimer timerWithStyle:ORBVisualTimerStyleBar frame:barFrame timeRemaining:(9)];
+                
+                barTimer.center = CGPointMake(containerBounds.size.width * 0.3,
+                                              containerBounds.size.height * 0.25);
+                
                 barTimer.barAnimationStyle = ORBVisualTimerBarAnimationStyleBackwards;
                 
                 int random = ((int)(5.0f * (i+1)) % 2) + 1;
@@ -143,13 +176,13 @@
                                                     [UIImage imageNamed:@"pattern_3"]];
                 
                 barTimer.backgroundViewColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.5 alpha:1.0f];
-                barTimer.backgroundViewCornerRadius = 50.0f;
+                barTimer.backgroundViewCornerRadius = 15.0f;
                 //barTimer.timerLabelColor = [UIColor whiteColor];
                 barTimer.showTimerLabel = NO;
                 //barTimer.autohideWhenFired = YES;
-                //barTimer.barCapStyle = kCALineCapSquare;
-                barTimer.barThickness = 60.0f;
-                barTimer.barPadding = 50.0f;
+                barTimer.barCapStyle = kCALineCapSquare;
+                barTimer.barThickness = containerBounds.size.height * 0.25 * 0.9;
+                barTimer.barPadding = 40.0f;
                 
                 break;
             }
@@ -158,10 +191,13 @@
                 break;
         }
         
+        barTimer.delegate = self;
+        barTimer.tag = i;
+        
         [barTimer addObserver:self forKeyPath:@"timeRemaining"
                       options:(NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld) context:nil];
         
-        [container addSubview:barTimer];
+        [self.sampleViews[i] addSubview:barTimer];
         [barTimer start];
     }
 }
@@ -186,7 +222,7 @@
                     bar.timerShapeActiveColor = [UIColor redColor];
                     
                     CGFloat barThickness = bar.barThickness;
-                    bar.barThickness += 4.0f;
+                    bar.barThickness += 2.0f;
                     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                         bar.timerLabelColor = barLabelColor;
                         bar.timerShapeActiveColor = barColor;
@@ -195,7 +231,7 @@
                         if (timeRemaining <= 0) {
                             bar.timerShapeInactiveColor = [UIColor redColor];
                             bar.barThickness += 10.0f;
-                            bar.barPadding += 20.0f;
+                            //bar.barPadding += 10.0f;
                         }
                     });
                 }
@@ -214,8 +250,8 @@
                 bar.timerShapeActiveColor = color;
                 
                 if (timeRemaining <= 0) {
-                    bar.backgroundViewColor = color;
-                    bar.barThickness -= 30.0f;
+                    //bar.backgroundViewColor = color;
+                    bar.barThickness -= bar.barThickness;
                     bar.barPadding += 60.0f;
                     bar.showTimerLabel = YES;
                 }
@@ -231,7 +267,9 @@
                     bar.timerShapeInactiveColor = [UIColor colorWithPatternImage:
                                                    [UIImage imageNamed:pattern]];
                     
-                    bar.barThickness += 30.0f;
+                    bar.barThickness += 10.0f;
+                    bar.barCapStyle = kCALineCapRound;
+                    bar.backgroundViewColor = [UIColor clearColor];
                     bar.barPadding -= 1.0f;
                 }
                 
